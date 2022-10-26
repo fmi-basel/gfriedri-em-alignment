4dc5b423aed228ce7f9860bfd3939780698e3c25
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2022-10-26 11:31:33 +0200

    Add info test.

---
+++ tests/record/test_Info.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+from sbem.record_v2.Info import Info
+
+
+class InfoTest(TestCase):
+    def test_info(self):
+        info = Info("name", "license")
+
+        assert info.get_format_version() == "0.1.0"
+        assert info.get_name() == "name"
+        assert info.get_license() == "license"
