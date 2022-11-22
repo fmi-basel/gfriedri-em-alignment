8235f0677eec2b605f55a714bdb8a8b965db6160
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2022-11-22 16:27:30 +0100

    Add is_initialized decorator.

--- src/sbem/record/Section.py
+++ src/sbem/record/Section.py
@@ -330,9 +330,11 @@ class Section(Info):
 
         return sec
 
+    @_Decorator.is_initialized
     def get_alignment_mesh(self) -> str:
         return self._alignment_mesh
 
+    @_Decorator.is_initialized
     def set_alignment_mesh(self, path: str):
         self._alignment_mesh = path
 
